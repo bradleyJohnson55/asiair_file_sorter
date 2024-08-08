2dc960af18758827cf1a5f69f52f3399e3253535
Brad Johnson <bjohnson@greenstreet.com>
2024-08-08 11:14:25 -0700

    add functionality to sort lights by date

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil, os, re
+import shutil
+import os
+import re
 from pathlib import Path
+from datetime import date, timedelta
 
 import config
 
@@ -37,12 +40,25 @@ def get_type(f):
 def get_date(f):
     """
     parses date from file
+
+    notes: 
+        - this will combine dates to a single date when the ran began
+          - e.g. if a run starts on 1/15/2020 and continues to the morning of 1/16/2020, they will go to a folder 2020-01-15
     """
 
-    search = re.search("_[0-9]{8}-", f)
-    if search:
-        date = f[search.start()+1:search.start()+9]
-        return date
+    date_search = re.search("_[0-9]{8}-", f)
+    if date_search:
+        dt = f[date_search.start()+1:date_search.start()+9]
+
+        time_search = re.search("_[0-9]{8}-[0-9]{4}", f)
+        time = time_search.group(0).split('-')[-1]
+
+        if int(time[:2]) < 10: # if time is morning
+            dt = date(int(dt[:4]), int(dt[4:6]), int(dt[6:]))
+            dt = dt - timedelta(days=1)
+            dt = dt.strftime('%Y%m%d')
+
+        return dt
     else:
         print(f"Could not parse date from file: {f}")
         
@@ -79,6 +95,7 @@ def init_project_structure(p):
 
 def main():
     project = input('Enter the name of the project directory: ')
+    # project = 'crescent_nebula'
     project_base_dir = f"{HOME_DIRECTORY}/{project}"
     init_project_structure(project_base_dir)
 
@@ -95,7 +112,9 @@ def main():
 
         # process Lights
         if get_type(f) == 'Light':
-            move_create(f"{project_base_dir}/{SCAN_DIR}/{f}", f"{project_base_dir}/Lights/{filter}")
+            date = get_date(f)
+            date = f"{date[:4]}-{date[4:6]}-{date[6:]}"
+            move_create(f"{project_base_dir}/{SCAN_DIR}/{f}", f"{project_base_dir}/Lights/{filter}/{date}")
         # process flats
         elif get_type(f) == 'Flat':
             # parse date from file
